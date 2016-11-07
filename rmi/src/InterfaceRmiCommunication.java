8dc29daddcf1d8ce4bb3bb9b15dd3c4c18eff259
Emmanuel <Emmanuel@LAPTOP-5A392FF7>
2016-11-06 23:02:33 -0600

    Se añaden clases al ejemplo

---
+++ rmi/src/InterfaceRmiCommunication.java
@@ -0,0 +1,10 @@
+
+
+
+
+import java.rmi.*;
+
+public interface InterfaceRmiCommunication extends Remote 
+{
+    Integer getSuma(int num1, int num2) throws RemoteException; 
+}
---
+++ rmi/src/RMIService.java
@@ -0,0 +1,32 @@
+
+
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.rmi.RemoteException;
+import java.security.spec.InvalidKeySpecException;
+
+public class RMIService {
+
+    //Operational Classes
+
+    private static RMIManager _rmiManager;
+    private static final int RMIPort = 1234;
+
+    //Constructors
+    public RMIService() throws InvalidKeySpecException, IOException, Exception {
+        try (ServerSocket serverSocket = new ServerSocket(RMIPort)) {
+            System.out.println("puerto disponible...");
+            serverSocket.close();
+            this._rmiManager = new RMIManager(RMIPort);
+
+        } catch (IOException x) {
+            System.out.println("el puerto esta ocupado..." + x);
+        }
+    }
+
+    public int getSuma(int num1, int num2) throws RemoteException {
+        Integer _translationRule = _rmiManager.getSuma(num1, num2);
+        return _translationRule;
+    }
+}
